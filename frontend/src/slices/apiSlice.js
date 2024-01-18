b903128b53b34f33f7eeeecce3c97a31bb7218bd
Hanif1201 <adeyemihanif@gmail.com>
2024-01-18 17:42:17 +0100

    redux toolkit

---
+++ frontend/src/slices/apiSlice.js
@@ -0,0 +1,17 @@
+import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
+import { BASE_URL } from "../constants";
+
+// NOTE: code here has changed to handle when our JWT and Cookie expire.
+// We need to customize the baseQuery to be able to intercept any 401 responses
+// and log the user out
+// https://redux-toolkit.js.org/rtk-query/usage/customizing-queries#customizing-queries-with-basequery
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: BASE_URL,
+});
+
+export const apiSlice = createApi({
+  baseQuery,
+  tagTypes: ["Product", "Order", "User"],
+  endpoints: (builder) => ({}),
+});
